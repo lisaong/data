4801d7a5477f3382cc47a2e8afde7be9881c22f6
Lisa Ong <lisaong@nus.edu.sg>
2018-03-12 16:59:10 +0800

    Refreshed data

--- tscs/generate.cmd
+++ tscs/generate.cmd
@@ -1,3 +1,6 @@
+REM Pre-requisites: Use Acrobat DC "Actions" to convert pdf to docx
+REM Example: (py36) generate.cmd d:\laughing-meme "d:\skillsmap\SSG - Skills Framework"
+
 set scripts_path=%1
 set folder_path=%2
 python %scripts_path%\scripts\doc_to_tsc_json.py %folder_path%
